6470f658eb50b550b0ff877d09210256b0f41fc6
assignnick <assignnik@gmail.com>
2018-11-29 07:23:31 +0400

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,9 +14,9 @@ public class Main {
         long m;
         long rez;
 
-        long AAAAAAALLLEEE;
-        AAAAAAALLLEEE=0L;
-        System.out.print(AAAAAAALLLEEE);
+        long AAAAAAALLEEELEEE;
+        AAAAAAALLEEELEEE=0L;
+        System.out.print(AAAAAAALLEEELEEE);
 
         long bl, wt;
         long x1, x2, x3, x4, y1, y2, y3, y4;
