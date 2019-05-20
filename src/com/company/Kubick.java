e148eb0f79a2fa4f6e0a76ab33be342ec34c4474
assignnick <assignnick@gmail.com>
2019-05-20 14:13:03 +0400

    1

--- src/com/company/Kubick.java
+++ src/com/company/Kubick.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Kubick {
+public class    Kubick {
 
 
     public static void main(String[] args) throws IOException {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,44 +1,161 @@
 package com.company;
 
 
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        //Scanner scanner = new Scanner("C:\\Users\\Vladimir\\IdeaProjects\\TestLabALG\\src\\text.txt");
-        int n = scanner.nextInt();
-        int[] mas = new int[n];
-        mas[0]=0;
-        int last;
-        int temp;
-        long answer = 0, maxanswer = 1;
-        last = scanner.nextInt();
-        int lastSign=-2;
-        for (int j = 1; j < mas.length; j++) {
-            temp = scanner.nextInt();
-            mas[j] = temp - last;
-            last = temp;
+    public static void main(String[] args) throws IOException {
+        //gjgjgjgjghjgjghjgjgjghjgjgjgjgjgjghjgjghjhjghjgghjghjghjghjghjgjghjgjghjghjghjgjgjghjgjghjgjghjghjghgjgj
+        Scanner scanner = new Scanner(new FileInputStream("input.txt"));
+        char c;
+        int zero = 0, one = 0, two = 0;
+
+
+        int aga=0;
+for (int x=1000;x<10000;x++) {
+    //  int x = 660;
+    double sqrt = Math.sqrt(x);
+    int currentValue = x;
+    int multiplier = 2;
+    int NumberOfMnojitel = 0;
+    while (currentValue > 1 && multiplier <= sqrt) {
+        if (currentValue % multiplier == 0) {
+          //  System.out.print(multiplier + " ");
+            currentValue /= multiplier;
+            NumberOfMnojitel++;
+        } else if (multiplier == 2) {
+            multiplier++;
+        } else {
+            multiplier += 2;
         }
-        for (int j = 0; j < mas.length; j++) {
-            if (lastSign==mas[j]) {
-                if(answer>maxanswer)
-                    maxanswer=answer;
-                answer = 0;
-            }
-            answer++;
-
-            if(mas[j]!=0)
-                lastSign=mas[j];
-        }
-
-
-        if(answer>maxanswer)
-            maxanswer=answer;
-        System.out.println(Arrays.toString(mas));
-        System.out.println(maxanswer+1);
+    }
+    if (currentValue != 1) {
+      //  System.out.println(currentValue);
+        NumberOfMnojitel++;
+    }
+   // System.out.println();
+   // System.out.println(NumberOfMnojitel);
+    if(NumberOfMnojitel!=1 && NumberOfMnojitel%2==1) {
+        System.out.print(x + " ");
+        System.out.println(aga);
+        aga++;
     }
 }
+
+
+
+
+        StringBuilder sb = new StringBuilder();
+
+        // BufferedWriter writer = new BufferedWriter(new FileWriter("text.txt"));
+        // writer.write("6 1 3\n012022");
+        //  writer.close();
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+        //   BufferedReader buffer = new BufferedReader(new FileReader("input.txt"));
+
+        int n = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        String str = scanner.next();
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (c == '0')
+                zero++;
+            if (c == '1')
+                one++;
+            if (c == '2')
+                two++;
+            sb.append(c);
+        }
+        if (n < a + b) {
+            writer.write("-1");
+            writer.close();
+        } else {
+            int mustOne;
+            int mustZero;
+            int mustTwo;
+            int answer = 0;
+            mustZero = a - zero;
+            mustOne = b - one;
+            mustTwo = (n - a - b)- two;
+
+//            System.out.println(sb.toString());
+//            System.out.println("mustZero " + mustZero);
+//            System.out.println("mustOne " + mustOne);
+//            System.out.printf("mustTwo %d%n%n", mustTwo);
+
+
+            for (int i = 0; i < sb.length(); ++i) {
+                c = sb.charAt(i);
+                if (c == '0') {
+                    if (mustZero < 0) {
+                        if (mustOne > 0) {
+                            sb.replace(i, i + 1, "1");
+                            mustOne--;
+                            mustZero++;
+                            answer++;
+                        } else if (mustTwo > 0) {
+                            sb.replace(i, i + 1, "2");
+                            mustZero++;
+                            mustTwo--;
+                            answer++;
+                        }
+                    }
+                }
+                if (c == '1') {
+                    if (mustOne < 0) {
+                        if (mustZero > 0) {
+                            sb.replace(i, i + 1, "0");
+                            mustZero--;
+                            mustOne++;
+                            answer++;
+                        } else if (mustTwo > 0) {
+                            sb.replace(i, i + 1, "2");
+                            mustTwo--;
+                            mustOne++;
+                            answer++;
+                        }
+                    }
+                }
+                if (c == '2') {
+                    if (mustTwo < 0) {
+                        if (mustZero > 0) {
+                            sb.replace(i, i + 1, "0");
+                            mustZero--;
+                            mustTwo++;
+                            answer++;
+                        } else if (mustOne > 0) {
+                            sb.replace(i, i + 1, "1");
+                            mustOne--;
+                            mustTwo++;
+                            answer++;
+                        }
+                    }
+                }
+//                if(i==sb.length()-1&&mustZero!=0&&mustOne!=0&&mustTwo!=0)
+//                    i=-1;
+
+            }
+
+            writer.write(answer + "\n" + sb.toString());
+            writer.close();
+//            System.out.println(answer);
+//            System.out.println(sb.toString());
+//            System.out.println("mustZero " + mustZero);
+//            System.out.println("mustOne " + mustOne);
+//
+//            System.out.println("mustTwo " + mustTwo);
+//
+//            System.out.println("Zero " + zero);
+//            System.out.println("one " + one);
+//            System.out.println("two " + two);
+        }
+
+    }
+}
+
+
 //        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 //        InputStreamReader input = new InputStreamReader(System.in);
 //        BufferedReader buffer = new BufferedReader(input);
