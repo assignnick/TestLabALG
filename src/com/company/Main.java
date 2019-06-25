57eec2f764a7173871f549c8427672fb646d9ca0
assignnick <assignnik@gmail.com>
2019-06-25 21:19:37 +0400

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,147 +1,104 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static ArrayList<ArrayList<Integer>> maxcircle = new ArrayList<>();
-    private static ArrayList<ArrayList<Integer>> circle = new ArrayList<>();
     private static ArrayList<Integer> notes = new ArrayList<>();
-    private static int[][] answer;
-    private static int[][] stations;
+    private static ArrayList<Integer>[] opyat;
+    private static int[][] graph;
+    private static int[][] cities;
+    private static boolean node = false;
+    private static boolean сfound = false;
+    private static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bi.readLine());
-        //int[][] mas = new int[n + 1][n + 1];
-        int[][] graph = new int[n][2];
-        int[] empty = new int[n];
-        for (int k = 0; k < empty.length; k++) {
-            empty[k] = -1;
-        }
-        answer = new int[n][2];
-        Map<Integer, Integer> map = new TreeMap<>();
-        int a, b;
-        for (int i = 0; i < n; i++) {
-            String str = bi.readLine();
-            StringTokenizer strtk = new StringTokenizer(str, " ");
+        // bi = new BufferedReader(new InputStreamReader(new FileInputStream("E:\\Users\\assig\\IdeaProjects\\AnotherTestAlgLab\\src\\com\\company\\gryadka.txt")));
+        String str = bi.readLine();
+        StringTokenizer strtk = new StringTokenizer(str, " ");
+        int n = (Integer.parseInt(strtk.nextToken()));
+        int k = (Integer.parseInt(strtk.nextToken()));
+        int a;
+        //answer = new int[n];
+        //opyat = new ArrayList[n];
+//        for (int i = 0; i < n; i++)
+//            opyat[i] = new ArrayList<>();
+        str = bi.readLine();
+        while (strtk.hasMoreTokens()) {
             a = (Integer.parseInt(strtk.nextToken()));
-            b = (Integer.parseInt(strtk.nextToken()));
-            for (int j = a; ; j = b) {
-                if (map.containsKey(j))
-                    map.put(j, map.get(j) + 1);
-                else
-                    map.put(j, 1);
-                if (j == b)
-                    break;
-            }
-//            mas[a][b] = 1;
-//            mas[b][a] = 1;
-            graph[i][0] = a;
-            graph[i][1] = b;
+            notes.add(a);
         }
-        //System.out.println(map.toString());
-        //System.out.println((Arrays.deepToString(graph)));
-        for (int k = 0; k < answer.length; k++) {
-            answer[k][0] = -1;
-            answer[k][1] = -1;
+        graph = new int[n][n];
+        answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            str = bi.readLine();
+            strtk = new StringTokenizer(str, " ");
+            for (int j = 0; j < n; j++) {
+                a = (Integer.parseInt(strtk.nextToken()));
+                graph[i][j] = a;
+            }
         }
-        for (Map.Entry<Integer, Integer> key : map.entrySet())
-            if (key.getValue() > 2) {
-                notes.add(key.getKey() - 1);
-                //System.out.println(key.getKey());
-            }
-        int round = 0;
-        if (notes.isEmpty())
-            for (int i = 0; i < n; i++)
-                System.out.print(0 + " ");
-        else {
-            for (int i : notes) {
-                stations = new int[n][2];
-                for (int k = 0; k < stations.length; k++) {
-                    stations[k][0] = -1;
-                    stations[k][1] = -1;
-                }
-                mydfs(i, graph, stations, i, -1, 0, round);
-                round++;
-                System.out.println((Arrays.deepToString(answer) + "ans"));
-                System.out.println((Arrays.deepToString(stations) + "stat"));
-                for (int j = 0; j < answer.length; j++) {
-                    if (stations[j][0] != -1 && stations[j][0] < answer[j][0] || answer[j][0] == -1)
-                        answer[j][0] = stations[j][0];
-                }
-                System.out.println((Arrays.deepToString(answer) + "ans"));
-            }
 
-            System.out.println(maxcircle.toString() + "cirkule");
-
-            ArrayList<Integer> zero = maxcircle.get(maxcircle.size() - 1);
-
-            int remov = zero.get(1) + 1;
-            int post = zero.get(1) + 1;
-            for (int j = maxcircle.size() - 1; j >= 0; j--) {
-                zero = maxcircle.get(j);
-                if ((zero.get(1) + 1) == (post)) {
-                    zero = maxcircle.get(j);
-                    remov = zero.get(0);
-                    post = zero.get(1);
-                    if (remov >= 0) {
-                        answer[remov][0] = 0;
-                        System.out.println(zero + " " + (remov + 1));
-                    }
-                }
-            }
-            //System.out.println();
-            for (int i = 0; i < answer.length; i++)
-                System.out.print(answer[i][0] + " ");
-        }
+        System.out.println(Arrays.deepToString(graph));
+//        for (int k = 0; k < answer.length; k++) {
+//            answer[k] = -1;
+//        }
+//        for (int i = 0; i < opyat.length; i++) {
+//            ArrayList li = opyat[i];
+//            if (li.size() > 2)
+//                notes.add(i);
+//        }
+//        cities = new int[n][2];
+//        if (notes.isEmpty())
+//            for (int i = 0; i < n; i++)
+//                System.out.print(0 + " ");
+//        else {
+//            for (int i : notes) {
+//                for (int k = 0; k < cities.length; k++) {
+//                    cities[k][0] = -1;
+//                    cities[k][1] = -1;
+//                }
+//                node = false;
+//                mydfs(i, i, -1, 0);
+//                if (node)
+//                    for (int j = 0; j < answer.length; j++) {
+//                        if ((cities[j][0] != -1 && cities[j][0] < answer[j]) || answer[j] == -1)
+//                            answer[j] = cities[j][0];
+//                    }
+//            }
+//            for (int i = 0; i < answer.length; i++)
+//                System.out.print(answer[i] + " ");
+//        }
     }
 
-    private static void mydfs(int station, int[][] graph, int[][] stations, int start, int pr, int fl, int round) {
-        if (stations[station][1] != round) {
-            circle.add(new ArrayList<Integer>(Arrays.asList(station, fl)));
-            if (stations[station][0] != -1) {
-                stations[station][0] = Math.min(fl, stations[station][0]);
-            } else {
-                stations[station][0] = fl;
-            }
-            stations[station][1] = round;
-            fl++;
-            for (int j = 0; j < graph.length; j++) {
-                for (int i = 0; i < graph[j].length; ++i) {
-                    if (graph[j][i] - 1 == station && graph[j][1 - i] - 1 != pr) {
-                        mydfs(graph[j][1 - i] - 1, graph, stations, start, station, fl, round);
+    private static void dfs(int station, int start, int pr, int fl) {
+        cities[station][1] = 1;
+        cities[station][0] = fl;
+        fl++;
+        for (int i = 0; i < opyat[station].size(); i++) {
+            int il = opyat[station].get(i);
+            for (int j = 0; j < graph[il].length; ++j) {
+                if ((graph[il][j] == station) && (graph[il][1 - j] != pr)) {
+                    int ik = graph[il][1 - j];
+                    if (cities[ik][1] == -1) {
+                        dfs(ik, start, station, fl);
+                    } else if (graph[il][1 - j] == start) {
+                        node = true;
+                        if (!сfound) {
+                            for (int jl = 0; jl < cities.length; jl++) {
+                                int[] ig = cities[jl];
+                                if (ig[1] == 1)
+                                    answer[jl] = 0;
+                            }
+                            сfound = true;
+                        }
                     }
                 }
             }
-        } else if (station == start) {
-            circle.add(new ArrayList<Integer>(Arrays.asList(station, fl)));
-            if (maxcircle.size() < circle.size())
-                maxcircle = circle;
-            circle = new ArrayList<>();
-            //stations[pr][0] = 0;
-//            if (!notes.contains(pr))
-//                for (int j = 0; j < graph.length; j++) {
-//                    for (int i = 0; i < graph[j].length; ++i) {
-//                        if (graph[j][i] - 1 == pr && graph[j][1 - i] - 1 != station) {
-//                            mydfs(graph[j][1 - i] - 1, graph, stations, start, station, 0, round);
-//                            break;
-//                        }
-//                    }
-//                }
-            answer = stations;
         }
-//        else {
-//            System.out.println("говножопа");
-//            for (int k = 0; k < stations.length; k++) {
-//                stations[k][0] = -1;
-//            }
-        System.out.println(Arrays.deepToString(stations) + "syk");
-        System.out.println(maxcircle.toString() + "cirkule");
-
-
+        cities[station][1] = 2;
     }
 }
+
 
---
+++ src/com/company/sixLaba/Metro.java
@@ -0,0 +1,97 @@
+/**
+ * https://codeforces.com/contest/131/problem/D
+ * D. Метро
+ */
+package com.company.sixLaba;
+import java.io.*;
+import java.util.*;
+
+public class Metro {
+    private static ArrayList<Integer> notes = new ArrayList<>();
+    private static ArrayList<Integer>[] opyat;
+    private static int[][] graph;
+    private static int[][] stations;
+    private static boolean node = false;
+    private static boolean сfound = false;
+    private static int[] answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bi.readLine());
+        answer = new int[n];
+        graph = new int[n][2];
+        opyat = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            opyat[i] = new ArrayList<>();
+        }
+        int a, b;
+        for (int i = 0; i < n; i++) {
+            String str = bi.readLine();
+            StringTokenizer strtk = new StringTokenizer(str, " ");
+            a = (Integer.parseInt(strtk.nextToken()));
+            b = (Integer.parseInt(strtk.nextToken()));
+            opyat[a - 1].add(i);
+            opyat[b - 1].add(i);
+            graph[i][0] = a - 1;
+            graph[i][1] = b - 1;
+        }
+        for (int k = 0; k < answer.length; k++) {
+            answer[k] = -1;
+        }
+        for (int i = 0; i < opyat.length; i++) {
+            ArrayList li = opyat[i];
+            if (li.size() > 2)
+                notes.add(i);
+        }
+        stations = new int[n][2];
+        if (notes.isEmpty())
+            for (int i = 0; i < n; i++)
+                System.out.print(0 + " ");
+        else {
+            for (int i : notes) {
+                for (int k = 0; k < stations.length; k++) {
+                    stations[k][0] = -1;
+                    stations[k][1] = -1;
+                }
+                node = false;
+                mydfs(i, i, -1, 0);
+                if (node)
+                    for (int j = 0; j < answer.length; j++) {
+                        if ((stations[j][0] != -1 && stations[j][0] < answer[j]) || answer[j] == -1)
+                            answer[j] = stations[j][0];
+                    }
+            }
+            for (int i = 0; i < answer.length; i++)
+                System.out.print(answer[i] + " ");
+        }
+    }
+
+    private static void mydfs(int station, int start, int pr, int fl) {
+        stations[station][1] = 1;
+        stations[station][0] = fl;
+        fl++;
+        for (int i = 0; i < opyat[station].size(); i++) {
+            int il = opyat[station].get(i);
+            for (int j = 0; j < graph[il].length; ++j) {
+                if ((graph[il][j] == station) && (graph[il][1 - j] != pr)) {
+                    int ik = graph[il][1 - j];
+                    if (stations[ik][1] == -1) {
+                        mydfs(ik, start, station, fl);
+                    } else if (graph[il][1 - j] == start) {
+                        node = true;
+                        if (!сfound) {
+                            for (int jl = 0; jl < stations.length; jl++) {
+                                int[] ig = stations[jl];
+                                if (ig[1] == 1)
+                                    answer[jl] = 0;
+                            }
+                            сfound = true;
+                        }
+                    }
+                }
+            }
+        }
+        stations[station][1] = 2;
+    }
+}
+
